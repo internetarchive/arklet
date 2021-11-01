423e1692149fc00072c69107c80f7c09cf18061d
Alex Dempsey <avdempsey@archive.org>
2021-10-31 23:03:46 -0700

    Add cProfiler middleware

--- arklet/settings.py
+++ arklet/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_cprofile_middleware.middleware.ProfilerMiddleware",
 ]
 
 ROOT_URLCONF = "arklet.urls"
