bf0edb80d41e1949f685f5c9e4645265fd65efe6
Alex Dempsey <avdempsey@archive.org>
2021-11-02 15:13:41 -0700

    Remove cProfiler middleware

--- arklet/settings.py
+++ arklet/settings.py
@@ -61,7 +61,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_cprofile_middleware.middleware.ProfilerMiddleware",
 ]
 
 ROOT_URLCONF = "arklet.urls"
