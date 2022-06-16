decd0080bd8e57261c5f962992229af710d4abfa
Alex Dempsey <avdempsey@archive.org>
2022-06-16 14:38:13 -0700

    Remove TimeLimitedPaginator since not DB agnostic
    TimeLimitedPaginator was used to make the ArkAdmin model load more
    quickly in Django Admin for large ARK tables. However, it uses a hack
    specific to Postgres. I removed it (it didn't make the admin very
    useful anyway) in favor of making Arklet DB agnostic.

--- ark/admin.py
+++ ark/admin.py
@@ -1,54 +1,46 @@
+"""Django Admin models for Arklet."""
+
 from django.contrib import admin
-from django.core.paginator import Paginator
-from django.db import OperationalError, connection, transaction
-from django.utils.functional import cached_property
 
 from ark.models import Ark, Key, Naan, Shoulder, User
 
 
-class TimeLimitedPaginator(Paginator):
-    """
-    Paginator that enforces a timeout on the count operation.
-    If the operations times out, a fake bogus value is
-    returned instead.
-
-    Lifted from: https://web.archive.org/web/20210422225156/https://hakibenita.com/optimizing-the-django-admin-paginator
-    """
-
-    @cached_property
-    def count(self):
-        # We set the timeout in a db transaction to prevent it from
-        # affecting other transactions.
-        with transaction.atomic(), connection.cursor() as cursor:
-            cursor.execute("SET LOCAL statement_timeout TO 1000;")
-            try:
-                return super().count
-            except OperationalError:
-                return 9999999999
-
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    """Django Admin model for ARK admin users."""
 
 
 @admin.register(Naan)
 class NaanAdmin(admin.ModelAdmin):
+    """Django Admin model for Name Assignment Authority Number bearing organizations."""
+
     list_display = ["name", "naan"]
 
 
 @admin.register(Shoulder)
 class ShoulderAdmin(admin.ModelAdmin):
+    """Django Admin model for ARK shoulders."""
+
     list_display = ["shoulder", "name", "naan"]
 
 
 @admin.register(Ark)
 class ArkAdmin(admin.ModelAdmin):
+    """Django Admin model for ARKs.
+
+    In practice, stock Django Admin doesn't work well for large randomly sorted tables.
+    This view will tend to take a long time to load as a full table count query is run.
+    """
+
     list_display = ["ark", "url"]
     show_full_result_count = False
-    paginator = TimeLimitedPaginator
 
 
 @admin.register(Key)
 class KeyAdmin(admin.ModelAdmin):
+    """Django Admin model for managing Arklet access keys.
+
+    These access keys are used to mint and bind ARKs via the Arklet API.
+    """
+
     list_display = ["key", "naan", "active"]
